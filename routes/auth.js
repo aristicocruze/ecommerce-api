91540a30eb3ab35359b7464b3fb5d09022955ae0
Aristides Cruz <aristidescruz02@gmail.com>
2021-11-22 10:44:05 +0100

    Complete login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,45 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //Register
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_KEY
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials");
+
+    //Decrypt password
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_KEY
+    );
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    //Check password
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials");
+
+    // Pass everything but password
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
