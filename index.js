ecdd3ea808fef2ed6921d5754255ec5dddf4adb2
Aristides Cruz <aristidescruz02@gmail.com>
2021-12-06 00:13:15 +0100

    Add stripe route

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -16,12 +18,14 @@ mongoose
   .catch(err => console.log(err));
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT, (req, res) => {
   console.log(`Backend running`);
